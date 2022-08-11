43b8eb27dc64a4a374087f839d0f0126559d5e90
NIBIN BABU <nibinbabu696@gmail.com>
2022-08-11 22:56:10 +0200

    Ui path Custom activity

---
+++ GetSum.cs
@@ -0,0 +1,25 @@
+﻿using RPA.Genie.Activities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathSum
+{
+    public class GetSum : Activity
+    {
+        public InProperty<int> FirstValue { get; set; }
+        public InProperty<int> SecondValue { get; set; }
+        public OutProperty<int> ResultSum { get; set; }
+        public override ExecutionResult Execute(IActivityContext activityContext)
+        {
+            int firstValue = FirstValue.GetValue(activityContext);
+            int secoundValue = SecondValue.GetValue(activityContext);
+
+            int result = firstValue + secoundValue;
+            ResultSum.SetValue(activityContext, result);
+            return ExecutionResult.Empty;
+        }
+    }
+}
